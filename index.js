f0663a1fd605d1b8385b386fc277e091684c4d6d
dancan <dancan.morara@student.moringaschool.com>
2025-03-15 18:14:28 +0300

    functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective ( star = "*") {
+    return function (adj = "special") {
+        return `You are ${star}${adj}${star}!`
+    }
+}
+console.log(wrapAdjective("||")("a hard worker"));
+wrapAdjective("||")("a dedicated programmer");
